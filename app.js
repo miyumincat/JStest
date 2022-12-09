d2cef6784045bd4d5303e587d5e6f9e2398614dc
soyoung <60204446+ysy3012000@users.noreply.github.com>
2022-12-09 23:02:01 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const palette = ['red', 'blue', 'green', 'tomato', 'white', 'black'];
+const calculator = {
+    plus: function(a, b) {
+        return a + b;
+    },
+    minus: function(a, b) {
+        return a - b;
+    },
+    times: function(a, b) {
+        return a * b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    },
+    power: function(a, b) {
+        return a ** b;
+    },
+};
 
-const tomoP = document.querySelector('#tomo');
-
-function changeColor() {
-  const randomColor = palette[Math.floor(Math.random() * palette.length)];
-  tomoP.style.color = randomColor;
-}
-
-tomoP.addEventListener('click', changeColor);
-console.log(tomoP);
-
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+const timeResult = calculator.times(10, minusResult);
---
+++ test3.js
@@ -0,0 +1,11 @@
+const palette = ['red', 'blue', 'green', 'tomato', 'white', 'black'];
+
+const tomoP = document.querySelector('#tomo');
+
+function changeColor() {
+    const randomColor = palette[Math.floor(Math.random() * palette.length)];
+    tomoP.style.color = randomColor;
+}
+
+tomoP.addEventListener('click', changeColor);
+console.log(tomoP);
